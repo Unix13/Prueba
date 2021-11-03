c78cc44f8445afc8771750d9cd7a3e00dac66485
Unix13 <vivasjesus80@gmail.com>
2021-11-03 08:52:36 +0100

    update

---
+++ Prueba/src/Prueba.java
@@ -0,0 +1,29 @@
+import utilidades.Entrada;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		
+		
+		int operando1,operando2;
+		double resultado;
+		
+		
+		System.out.println("Introduce operando 1");
+		operando1 = Entrada.entero();
+
+		System.out.println("Introduce operando 2");
+		operando2 = Entrada.entero();
+
+		if (operando2 == 0) {
+			do {
+				System.out.println("Has introducido un denominador 0");
+				System.out.println("Introduce operando 2");
+				operando2 = Entrada.entero();
+			} while (operando2 == 0);
+		}
+
+		resultado = operando1 % operando2;
+		System.out.println(resultado);
+	}
+}
